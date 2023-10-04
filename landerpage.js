ad834d428ce34ae276873cd7324448721ec738f3
Riddhilalla <riddhilalla16@gmail.com>
2023-10-04 23:15:23 +0530

    lander page is now home page

--- landerpage.js
+++ landerpage.js
@@ -1,58 +1,20 @@
 
-function toggleDropdown() {
-    var dropdown = document.getElementById("myDropdown");
-    dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
-}
-
-
-window.onclick = function(event) {
-    if (!event.target.matches('.dropdown')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        for (var i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.style.display === "block") {
-                openDropdown.style.display = "none";
-            }
-        }
+var swiper = new Swiper(".swiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2,
+      slideShadows: true
+    },
+    spaceBetween: 60,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
     }
-}
-const images = document.querySelectorAll('.images .img');
-
-
-let currentIndex = 0;
-
-
-function selectImage(index) {
-    currentIndex = index;
-    updateSelected();
-}
-
-function updateSelected() {
-    images.forEach((img, index) => {
-        if (index === currentIndex) {
-            img.classList.add('selected');
-        } else {
-            img.classList.remove('selected');
-        }
-    });
-}
-
-function prevImage() {
-    currentIndex--;
-    if (currentIndex < 0) {
-        currentIndex = images.length - 1;
-    }
-    updateSelected();
-}
-
-
-function nextImage() {
-    currentIndex++;
-    if (currentIndex >= images.length) {
-        currentIndex = 0;
-    }
-    updateSelected();
-}
-
-
-updateSelected();
+  });
